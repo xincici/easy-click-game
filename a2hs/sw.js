a608f165f63738dd9c5eb38a6df57bd6f8a0c718
xincici <llxy8687@foxmail.com>
2023-03-15 15:54:37 +0800

    modify sw.js

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,14 +1,4 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
-      '/media/'
-    ])),
-  );
+  console.log('install done');
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
