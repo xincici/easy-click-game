18c0abdbd5f958165431a18c14db08e91041590e
xincici <llxy8687@foxmail.com>
2023-03-15 16:35:19 +0800

    modify sw.js

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -2,3 +2,7 @@ self.addEventListener('install', (e) => {
   console.log('install done');
 });
 
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(fetch(e.request));
+});
