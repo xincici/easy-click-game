cc74393d62d6f12c60bc3b84db5e21a1c54bde5a
xincici <llxy8687@foxmail.com>
2023-03-20 10:13:35 +0800

    remove fetch event listener

--- sw.js
+++ sw.js
@@ -2,7 +2,3 @@ self.addEventListener('install', (e) => {
   console.log('install done');
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(fetch(e.request));
-});
