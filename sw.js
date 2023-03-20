b7b049a98853e767db04d880983f00d219857520
xincici <llxy8687@foxmail.com>
2023-03-20 11:10:34 +0800

    update sw.js with fetch activate listener

--- sw.js
+++ sw.js
@@ -1,4 +1,46 @@
-self.addEventListener('install', (e) => {
-  console.log('install done');
+
+const cacheName = 'easy-click-game-v1';
+const cacheFileList = [
+  '/index.html',
+  '/favicon.ico',
+  '/a2hs/fox.png',
+  '/a2hs/manifest.webmanifest',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching files');
+      return cache.addAll(cacheFileList);
+    })
+  );
 });
 
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetched resource ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: ' + e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: ' + e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if(cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
