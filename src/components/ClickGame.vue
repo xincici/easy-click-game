7fdd11f974148bedd4a06a110400f48429659542
xincici <llxy8687@foxmail.com>
2023-05-06 17:51:08 +0800

    feat: use canvas confetti

--- src/components/ClickGame.vue
+++ src/components/ClickGame.vue
@@ -42,13 +42,11 @@
 
 <script setup>
 import { ref, reactive, computed, watch, watchEffect } from 'vue';
-import JSConfetti from 'js-confetti';
+import confetti from 'canvas-confetti';
 
 import TopHeader from './TopHeader.vue';
 import { theme } from '../utils/theme';
 import { difficulty, changeDifficulty, MIN_DIFFICULTY, MAX_DIFFICULTY } from '../utils/difficulty';
-
-const jsConfetti = new JSConfetti();
 
 const BIG_VAL = 3;
 const VIRTUAL_CLICK_EFFECT_DURATION = 220;
@@ -90,7 +88,10 @@ watch(difficulty, initGame, { immediate: true });
 watch(gameResult, val => {
   if (val === WIN) {
     updateBestScore();
-    jsConfetti.addConfetti();
+    confetti({
+      particleCount: 200,
+      spread: 120
+    });
   }
 });
 
